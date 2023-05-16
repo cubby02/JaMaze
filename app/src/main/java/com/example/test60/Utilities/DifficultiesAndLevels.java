a584ac5e3f075351a9074cf4fc708b9d65e3f893
Jeeyk1 <nooojake@yahoo.com>
2023-05-16 20:54:12 +0800

    Unlock all levels and difficulties except easy level 1

--- app/src/main/java/com/example/test60/Utilities/DifficultiesAndLevels.java
+++ app/src/main/java/com/example/test60/Utilities/DifficultiesAndLevels.java
@@ -15,26 +15,26 @@ public class DifficultiesAndLevels {
 
         // Set the initial state for the levels
         editor.putBoolean("easy_unlocked", true);
-        editor.putBoolean("easy2_unlocked", true); // Easy level 1 is unlocked by default
-        editor.putBoolean("easy3_unlocked", true);
-        editor.putBoolean("easy4_unlocked", true);
-        editor.putBoolean("easy5_unlocked", true);
-        editor.putBoolean("average1_unlocked", true);
-        editor.putBoolean("average2_unlocked", true);
-        editor.putBoolean("average3_unlocked", true);
-        editor.putBoolean("average4_unlocked", true);
-        editor.putBoolean("average5_unlocked", true);
-        editor.putBoolean("hard1_unlocked", true);
-        editor.putBoolean("hard2_unlocked", true);
-        editor.putBoolean("hard3_unlocked", true);
-        editor.putBoolean("hard4_unlocked", true);
-        editor.putBoolean("hard5_unlocked", true);
-        editor.putBoolean("extreme1_unlocked", true);
-        editor.putBoolean("extreme2_unlocked", true);
-        editor.putBoolean("extreme3_unlocked", true);
-        editor.putBoolean("extreme4_unlocked", true);
-        editor.putBoolean("extreme5_unlocked", true);
-        editor.putBoolean("game_completed", true);
+        editor.putBoolean("easy1_unlocked", true); // Easy level 1 is unlocked by default
+        editor.putBoolean("easy2_unlocked", preferences.getBoolean("easy2_unlocked", false)); // Use the current value of easy2_unlocked
+        editor.putBoolean("easy3_unlocked", preferences.getBoolean("easy3_unlocked", false));
+        editor.putBoolean("easy4_unlocked", preferences.getBoolean("easy4_unlocked", false));
+        editor.putBoolean("easy5_unlocked", preferences.getBoolean("easy5_unlocked", false));
+        editor.putBoolean("average1_unlocked", preferences.getBoolean("average1_unlocked", false));
+        editor.putBoolean("average2_unlocked", preferences.getBoolean("average2_unlocked", false));
+        editor.putBoolean("average3_unlocked", preferences.getBoolean("average3_unlocked", false));
+        editor.putBoolean("average4_unlocked", preferences.getBoolean("average4_unlocked", false));
+        editor.putBoolean("average5_unlocked", preferences.getBoolean("average5_unlocked", false));
+        editor.putBoolean("hard1_unlocked", preferences.getBoolean("hard1_unlocked", false));
+        editor.putBoolean("hard2_unlocked", preferences.getBoolean("hard2_unlocked", false));
+        editor.putBoolean("hard3_unlocked", preferences.getBoolean("hard3_unlocked", false));
+        editor.putBoolean("hard4_unlocked", preferences.getBoolean("hard4_unlocked", false));
+        editor.putBoolean("hard5_unlocked", preferences.getBoolean("hard5_unlocked", false));
+        editor.putBoolean("hard5_unlocked", preferences.getBoolean("extreme1", false));
+        editor.putBoolean("hard5_unlocked", preferences.getBoolean("extreme2", false));
+        editor.putBoolean("hard5_unlocked", preferences.getBoolean("extreme3", false));
+        editor.putBoolean("hard5_unlocked", preferences.getBoolean("extreme4", false));
+        editor.putBoolean("hard5_unlocked", preferences.getBoolean("extreme5", false));
 
 
 
